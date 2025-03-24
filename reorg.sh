864ecf0f2965488f4bf4181dd23d4419f8392048
David Koosis <dkoosis@gmail.com>
2025-03-24 13:03:32 -0700

    imports

--- reorg.sh
+++ reorg.sh
@@ -1,4 +1,2 @@
-# Move the file to the right location
 mkdir -p test/unit/mcp
-cp test/helpers/mcp/resources_test.go test/unit/mcp/
-rm test/helpers/mcp/resources_test.go
+mv test/helpers/mcp/resources_test.go test/unit/mcp/
